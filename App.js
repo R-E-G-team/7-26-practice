23d4f11461d8bc105db4e3efd658810215d902d6
DongheonKim <oueya1479@naver.com>
2021-07-26 22:52:35 +0900

    Update App.js

--- App.js
+++ App.js
@@ -4,17 +4,47 @@ import React, { useState } from "react";
 import {
   StyleSheet,
   View,
-  Text
+  Text,
+  TextInput,
+  TouchableHighlight
 } from "react-native";
 
 export default function App() {
   return (
-    <View style={{padding: 20}}>
-      <Text>2</Text>
+    <View style={styles.container}>
+      <View>
+        <Text>당신의 계정을 입력하세요!</Text>
+      </View>
+      <TextInput style={styles.textInput} placeholder='id' value='id'></TextInput>
+      <TextInput style={styles.textInput} placeholder='password' value='password'></TextInput>
+      <TouchableHighlight style={styles.signIn}><Text>Sign In</Text></TouchableHighlight>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    padding: 20,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  textInput: {
+    width: '100%',
+    height: 50,
+    paddingLeft: 20,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: '#aaaaaa',
+    borderRadius: 20
+  },
+  signIn: {
+    backgroundColor: '#bbbbff',
+    borderRadius: 20,
+    width: '100%',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
