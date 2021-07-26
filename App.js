86ba3111e46767cd77c3d8cd043181b76d02d167
DongheonKim <oueya1479@naver.com>
2021-07-26 22:22:11 +0900

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+} from "react-native";
+
+export default function App() {
+  return ;
+}
+
+const styles = StyleSheet.create({
+});
