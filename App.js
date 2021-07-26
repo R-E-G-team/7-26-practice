3fc8b08a4ae2e5a834ff12e6110f4644363a8fd7
dorikoak <dori20051@gmail.com>
2021-07-26 23:03:39 +0900

    Update App.js

--- App.js
+++ App.js
@@ -6,18 +6,39 @@ import {
   View,
   Text,
   TextInput,
-  TouchableHighlight
+  TouchableHighlight,
 } from "react-native";
 
 export default function App() {
+  const [id, password] = useState("");
+  const [massage, setMassage] = useState("");
+
+  const checkingIdPW = () => {
+    if (id == "dori" && password == "1234") {
+      setMassage("");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View>
         <Text>당신의 계정을 입력하세요!</Text>
       </View>
-      <TextInput style={styles.textInput} placeholder='id' value='id'></TextInput>
-      <TextInput style={styles.textInput} placeholder='password' value='password'></TextInput>
-      <TouchableHighlight style={styles.signIn}><Text>Sign In</Text></TouchableHighlight>
+      <TextInput
+        style={styles.textInput}
+        placeholder="id"
+        value={id}
+        onChange={(value) => (id = value)}
+      ></TextInput>
+      <TextInput
+        style={styles.textInput}
+        placeholder="password"
+        value={password}
+        onChange={(value) => (password = value)}
+      ></TextInput>
+      <TouchableHighlight style={styles.signIn}>
+        <Text>Sign In</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -25,26 +46,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     padding: 20,
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center'
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
   },
   textInput: {
-    width: '100%',
+    width: "100%",
     height: 50,
     paddingLeft: 20,
     margin: 10,
     borderWidth: 1,
-    borderColor: '#aaaaaa',
-    borderRadius: 20
+    borderColor: "#aaaaaa",
+    borderRadius: 20,
   },
   signIn: {
-    backgroundColor: '#bbbbff',
+    backgroundColor: "#bbbbff",
     borderRadius: 20,
-    width: '100%',
+    width: "100%",
     height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
