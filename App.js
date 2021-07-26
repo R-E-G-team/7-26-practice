f0484b70d6ded9cce948e08b0dafa5844d464501
dorikoak <dori20051@gmail.com>
2021-07-26 22:28:11 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
+
 import {
   StyleSheet,
   View,
+  Text
 } from "react-native";
 
 export default function App() {
-  return ;
+  return (
+    <View style={{padding:20}}>
+      <Text>1</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
