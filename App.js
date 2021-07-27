9dd4f305a545968e80785cf3fe384deb545a967b
DongheonKim <oueya1479@naver.com>
2021-07-27 23:48:51 +0900

    Add screens sign up & in

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-
 import {
   StyleSheet,
   View,
@@ -9,67 +8,22 @@ import {
   TouchableOpacity,
 } from "react-native";
 
-export default function App() {
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
-  const [massage, setMassage] = useState("당신의 계정을 입력하세요!");
+import SignIn from "./screens/SignIn";
+import SignUp from "./screens/SignUp";
 
-  const checkingIdPW = () => {
-    if (id === "dori" && password === "1234") {
-      setMassage("로그인 성공");
-    }
-    else {
-      setMassage("로그인 실패");
-    }
+export default function App() {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const onClick = () => {
+    setIsLogin(!isLogin);
   };
 
   return (
-    <View style={styles.container}>
-      <View>
-        <Text>{message}</Text>
-      </View>
-      <TextInput
-        style={styles.textInput}
-        placeholder="id"
-        value={id}
-        onChangeText={(value) => setId(value)}
-      ></TextInput>
-      <TextInput
-        style={styles.textInput}
-        placeholder="password"
-        value={password}
-        onChangeText={(value) => setPassword(value)}
-      ></TextInput>
-      <TouchableOpacity style={styles.signIn} onPress={checkingIdPW}>
-        <Text>Sign In</Text>
-      </TouchableOpacity>
+    <View>
+        { isLogin && <SignIn onClick={onClick}/> }
+        { !isLogin && <SignUp onClick={onClick}/> }
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  textInput: {
-    width: "100%",
-    height: 50,
-    paddingLeft: 20,
-    margin: 10,
-    borderWidth: 1,
-    borderColor: "#aaaaaa",
-    borderRadius: 20,
-  },
-  signIn: {
-    backgroundColor: "#bbbbff",
-    borderRadius: 20,
-    width: "100%",
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
